472fd0d916a5189a7077623c0bb86bd5df25269a
hlg <public.void@gmx.de>
2024-12-06 21:04:39 +0100

    improve BIMserver url handling, try to validate url, handle trailing slash

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
-if (urlParam('dev')) window.sessionStorage.setItem('bimserverHost', 'http://localhost:8082');
-if (urlParam('bimserver')) window.sessionStorage.setItem('bimserverHost', urlParam('bimserver'));
+if (urlParam('dev')!=null) {
+  window.sessionStorage.setItem('bimserverHost', 'http://localhost:8082');
+} else {
+  var urlHost = urlParam('bimserver');
+  if (urlHost!=null){
+    if(urlHost!=''){
+      if(validateUrl(urlHost)){
+         window.sessionStorage.setItem('bimserverHost', urlHost.endsWith('/') ? urlHost.slice(0,-1) : urlHost);
+      }
+    } else {
+     window.sessionStorage.removeItem('bimserverHost')
+    }
+  }
+}
 var storedHost = window.sessionStorage.getItem('bimserverHost');
-var bimserverHost =  storedHost ?  storedHost : document.location.href.split('web')[0]+'bimserver';
+var guessedHost = document.location.href.includes('web') ? document.location.href.split('web')[0]+'bimserver' : '';
+var bimserverHost =  storedHost ?  storedHost : guessedHost ? guessedHost : '';
 require.config({
     urlArgs : null,
     baseUrl: 'js',
@@ -24,11 +37,15 @@ var notifier = {
     resetStatusQuick: function(){},
     clear: function(){}
 }
+
 function success(msg){
     return function(){ notifier.setSuccess(msg); };
 }
 function urlParam(name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results ? results[1] : false;
+    var results = new RegExp('[\?&]' + name + '(=([^&#]*))?').exec(window.location.href);
+    return results ? results[2] : null;
+}
+function validateUrl(url){
+  try{ new URL(url); } catch(e) {return false; } return true;
 }
 
